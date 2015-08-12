0de19ba3b8cf1db77a12472b151ab5744954f5e0
Greenstreet <tom_greenstreet1@hotmail.co.uk>
2015-08-12 12:03:21 +0100

    First commit

---
+++ Tetrimo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris {
+    class Tetrimo {
+
+        private char type;
+        private int[,] shape;
+
+        public Tetrimo(char type) {
+            this.type = type;
+            this.shape = getShapeDirective(type); 
+        }
+
+        private int[,] getShapeDirective(char type) {
+            int[,] drawInstruction;
+
+            switch (type) {
+                case 'o':
+                    return drawInstruction = new int[,] { { 0, 0, 0, 0 }, { 0, 1, 1, 0 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 } };
+                case 'z':
+                    return drawInstruction = new int[,] { { 0, 0, 0, 0 }, { 0, 1, 1, 0 }, { 0, 0, 1, 1 }, { 0, 0, 0, 0 } };
+                case 't':
+                    return drawInstruction = new int[,] { { 0, 0, 0, 0 }, { 0, 1, 1, 1 }, { 0, 0, 1, 0 }, { 0, 0, 0, 0 } };
+                case 'i':
+                    return drawInstruction = new int[,] { { 0, 0, 0, 0 }, { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+                case 's':
+                    return drawInstruction = new int[,] { { 0, 0, 0, 0 }, { 0, 0, 1, 1 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 } };
+                case 'l':
+                    return drawInstruction = new int[,] { { 0, 0, 0, 0 }, { 0, 1, 1, 1 }, { 0, 1, 0, 0 }, { 0, 0, 0, 0 } };
+                case 'j':
+                    return drawInstruction = new int[,] { { 0, 0, 0, 0 }, { 0, 1, 1, 1 }, { 0, 0, 0, 1 }, { 0, 0, 0, 0 } };
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ Tetris.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Tetris{
+
+    class Tetris{
+
+        public static int cursorX = 1;
+        public static int cursorY = 1;
+
+        static void Main(string[] args){
+
+            Console.WindowHeight = 35;
+            Console.WindowWidth = 45;
+
+            Border b1 = new Border(0, 0, ConsoleColor.White);
+            b1.drawBorder();
+
+            Engine();
+            //Console.ForegroundColor = ConsoleColor.Cyan;
+
+        }
+
+        private static void Engine(){
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTick);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+            int[,] tetrisBoard = new int[10, 20];
+            
+
+            while (true){
+
+                if (Console.KeyAvailable){
+                    ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+
+                    if (keyPressed.Key == ConsoleKey.LeftArrow){
+                        //Console.SetCursorPosition(4, 2);
+                        Console.WriteLine("██");
+                    }
+                    if (keyPressed.Key == ConsoleKey.RightArrow) {
+                        //Console.SetCursorPosition(4, 2);
+                        Console.WriteLine("██");
+                    }
+                    if (keyPressed.Key == ConsoleKey.DownArrow) {
+                        //Console.SetCursorPosition(4, 2);
+                        Console.WriteLine("██");
+                    }
+                    if (keyPressed.Key == ConsoleKey.UpArrow) {
+                        //Console.SetCursorPosition(4, 2);
+                        Console.WriteLine("██");
+                    }
+                }
+            }
+        }
+
+        private void adjustCursorLocation(int cursorX, int cursorY) {
+
+        }
+
+        private static void OnTick(object source, ElapsedEventArgs e) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
